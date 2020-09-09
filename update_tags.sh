e7923f8ab299a67c4728307b8390a62b8bb630be
Manoj Kumar MC <mmyali26@in.ibm.com>
2020-09-09 15:43:20 +0530

    setting up ctags for golang

---
+++ update_tags.sh
@@ -0,0 +1,16 @@
+# Update the following snippet to ~/.ctags
+# --langdef=Go
+# --langmap=Go:.go
+# --regex-Go=/func([ \t]+\([^)]+\))?[ \t]+([a-zA-Z0-9_]+)/\2/d,func/
+# --regex-Go=/var[ \t]+([a-zA-Z_][a-zA-Z0-9_]+)/\1/d,var/
+# --regex-Go=/type[ \t]+([a-zA-Z_][a-zA-Z0-9_]+)/\1/d,type/
+
+rm -rf ./tags
+echo "tag file removed"
+ctags -f tags --exclude="*test*" --exclude="*.js" \
+  -R $GOPATH/src/github.ibm.com/MCMP-Development \
+  -R $GOPATH/src/github.ibm.com/MCMP-Discovery
+echo "tag file updated"
+
+# update vimrc with the path
+# set tags=./tags,tags,/home/user/commontags
